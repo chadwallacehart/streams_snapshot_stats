c2eacdee0e38be1bc06d21fc2ef527a3258ce2fc
Chad Hart <chadwallacehart>
2019-03-11 13:57:31 +0200

    updates I forgot to push

--- getLogs_stats.py
+++ getLogs_stats.py
@@ -16,46 +16,55 @@ def process_getLogs(input_json_file, agentid='Default', print_output=False):
 
     for l, entry in enumerate(json_data):
         if 'objects' in entry:
-            if entry['text'] == "GET_AGENT_SNAPSHOT succeeded.":
-                snapshot = entry['objects'][0]['snapshot']
-                # snapshot_data.append(snapshot)
+            if entry['text'] != "GET_AGENT_SNAPSHOT succeeded.":
+                continue
 
-                data = {}
+            snapshot = entry['objects'][0]['snapshot']
+            # snapshot_data.append(snapshot)
 
-                if 'state' in snapshot:
-                    data = {
-                        'agentid': agentid,
-                        'linenum': l,
-                        'state_time': dateutil.parser.parse(snapshot['state']['startTimestamp']),
-                        'state_time_str': snapshot['state']['startTimestamp'],
-                        'state_type': snapshot['state']['type'],
-                        'agent_state': snapshot['state']['name'],
-                    }
+            data = {}
 
-                for contact in snapshot['contacts']:
+            if 'state' in snapshot:
+                data = {
+                    'agentid': agentid,
+                    'linenum': l,
+                    'state_time': dateutil.parser.parse(snapshot['state']['startTimestamp']),
+                    'state_time_str': snapshot['state']['startTimestamp'],
+                    'state_type': snapshot['state']['type'],
+                    'agent_state': snapshot['state']['name'],
+                }
 
-                    if 'state' in contact:
-                        data['agent_in_call_state'] = contact['state']['type']
-                        data['agent_call_state_time'] = dateutil.parser.parse(contact['state']['timestamp'])
-                        data['agent_call_state_time_str'] = contact['state']['timestamp']
+            for contact in snapshot['contacts']:
 
-                    if 'queue' in contact:
-                        data['queue'] = contact['queue']['name']
+                data['contact_id'] = contact['contactId']
 
-                    data['connections'] = -1
+                if 'state' in contact:
+                    data['agent_in_call_state'] = contact['state']['type']
+                    # data['agent_in_call_state'] = contact['state']['name']
+                    data['agent_call_state_time'] = dateutil.parser.parse(contact['state']['timestamp'])
+                    data['agent_call_state_time_str'] = contact['state']['timestamp']
 
-                    for i, connection in enumerate(contact['connections']):
-                        key = "connection%i" % i
-                        data['connections'] += 1
-                        data['%s_direction' % key] = connection['type']
-                        data['%s_state' % key] = connection['state']['type']
-                        data['%s_timestamp_str' % key] = connection['state']['timestamp']
-                        data['%s_timestamp' % key] = dateutil.parser.parse(connection['state']['timestamp'])
-                        if 'endpoint' in connection:
-                            if connection['endpoint']['phoneNumber']:
-                                data['%s_phone' % key] = connection['endpoint']['phoneNumber']
+                if 'queue' in contact:
+                    data['queue'] = contact['queue']['name']
 
-                snapshot_data.append(data)
+                data['connections'] = -1
+
+                for i, connection in enumerate(contact['connections']):
+                    key = "connection%i" % i
+                    data['connections'] += 1
+                    data['%s_id' % key] = connection['connectionId']
+                    data['%s_direction' % key] = connection['type']
+                    data['%s_state' % key] = connection['state']['type']
+                    data['%s_timestamp_str' % key] = connection['state']['timestamp']
+                    data['%s_timestamp' % key] = dateutil.parser.parse(connection['state']['timestamp'])
+                    if 'endpoint' in connection:
+                        if connection['endpoint']['type']:
+                            data['%s_phone' % key] = connection['endpoint']['type']
+
+                        if connection['endpoint']['phoneNumber']:
+                            data['%s_phone' % key] = connection['endpoint']['phoneNumber']
+
+            snapshot_data.append(data)
 
     print("lines processed %i" % len(snapshot_data))
 
@@ -75,19 +84,20 @@ def process_getLogs(input_json_file, agentid='Default', print_output=False):
         if snapshot_data[i]['agent_state'] != snapshot_data[i + 1]['agent_state']:
             td = ((snapshot_data[i + 1]['state_time'] - snapshot_data[i]['state_time']).total_seconds())
 
-
-
             data = {
                 'agentid': entry['agentid'],
-                'type': 'agent',
+                'metric_type': 'agent_status',
                 'state': entry['agent_state'],
                 'state_time': snapshot_data[i]['state_time_str'],
                 'duration': td,
             }
 
             if 'connections' in entry:
+                data['connection_type'] = entry['connection0_direction']
+
                 num_connections = entry['connections']
                 data['connections'] = num_connections
+
                 if num_connections == 1:
                     data['direction'] = entry['connection1_direction']
                 elif num_connections == 2:
@@ -107,6 +117,14 @@ def process_getLogs(input_json_file, agentid='Default', print_output=False):
         for i in state_change_data:
             print(i)
 
+
+    # ToDo: run through and calculate state times for each of the following:
+    # ~agent_state~, ~agent_in_call_state~, connection0_state, connection1_state, connection2_state
+    # currently it just looks at agent_state and agent_in_call_state;
+    # State times are only calculated properly for agent_state and agent_in_call_state;
+    # need ot rerun the loop for each state type
+
+
     ### Get in-call states
 
     # filter and sort just the in_call_states
@@ -118,6 +136,7 @@ def process_getLogs(input_json_file, agentid='Default', print_output=False):
             break
 
         if in_call_states[i]['agent_in_call_state'] != in_call_states[i + 1]['agent_in_call_state']:
+
             td = ((in_call_states[i + 1]['agent_call_state_time'] - in_call_states[i][
                 'agent_call_state_time']).total_seconds())
 
@@ -125,12 +144,15 @@ def process_getLogs(input_json_file, agentid='Default', print_output=False):
 
             data = {
                 'agentid': entry['agentid'],
-                'type': 'in_call',
+                'metric_type': 'in_call',
                 'state': entry['agent_in_call_state'],
                 'state_time': in_call_states[i]['agent_call_state_time_str'],
                 'duration': td,
                 'connections': num_connections
             }
+
+            if entry['connection0_direction']:
+                data['connection_type'] = entry['connection0_direction']
 
             if num_connections == 1:
                 data['direction'] = entry['connection1_direction']
@@ -145,7 +167,7 @@ def process_getLogs(input_json_file, agentid='Default', print_output=False):
     if print_output:
         print("AGENT IN CALL STATE TIMES")
         for i in state_change_data:
-            if i['type'] == 'in_call':
+            if i['metric_type'] == 'in_call':
                 print(i)
 
     state_change_data = sorted(state_change_data, key=operator.itemgetter('state_time'))
@@ -159,12 +181,13 @@ def output_csv(filename, state_change_data, snapshot_data, append=False):
     state_change_file = "%s_states.csv" % filename
 
     if append:
-        file_permissions = 'w+'
+        file_permissions = 'a'
     else:
         file_permissions = 'w'
 
+    # write the state changes to one file
     with open(state_change_file, file_permissions, newline='') as csvfile:
-        fieldnames = ['type', 'state', 'state_time', 'duration']
+        fieldnames = ['agentid', 'state_time', 'metric_type', 'connection_type', 'direction', 'state', 'duration', 'connections']
         writer = csv.DictWriter(csvfile, dialect='excel', fieldnames=fieldnames)
         if not append:
             writer.writeheader()
@@ -186,6 +209,7 @@ def output_csv(filename, state_change_data, snapshot_data, append=False):
     for field in snapshot_data:
         get_unique(field)
 
+    # write all agent snapshots to this file
     with open(snapshot_file, file_permissions, newline='') as csvfile:
         writer = csv.DictWriter(csvfile, dialect='excel', fieldnames=unique_keys )
         if not append:
